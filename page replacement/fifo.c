a86b9bd9505ed2ab07b63b0fbc2d4ffc15de2ee2
diyajojo <diyaj2122@gmail.com>
2025-04-02 21:11:43 +0530

    lru page replacement

--- page replacement/fifo.c
+++ page replacement/fifo.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-#define FRAME_SIZE 3;
+#define FRAME_SIZE 3
 
 int frames[FRAME_SIZE];
 int found=0;
+int rear=-1;
+int page_fault=0;
 
 void initialize()
 {
@@ -13,6 +15,22 @@ void initialize()
     }
 }
 
+void displayFrames()
+{
+    for(int i=0;i<FRAME_SIZE;i++)
+    {
+        if(frames[i]==-1)
+        {
+            printf("%d\t",-1);
+        }
+        else
+        {
+            printf("%d\t",frames[i]);
+        }
+    }
+    printf("\n");
+}
+
 void fifo(int n,int pages[])
 {
    for(int i=0;i<n;i++)
@@ -20,10 +38,21 @@ void fifo(int n,int pages[])
     int page=pages[i];
     found=0;
 
-    for(int i=0;i<FRAME_SIZE;i++)
+    for(int j=0;j<FRAME_SIZE;j++)
     {
-        
+       if(frames[j]==page) 
+       {
+           found=1;
+           break;
+       }
     }
+    if(found==0)
+    {
+        rear=(rear+1)%FRAME_SIZE;
+        frames[rear]=page;
+        page_fault++;
+    }
+    displayFrames();
    }
 }
 
@@ -32,16 +61,16 @@ int main()
     int n,pages[20];
 
     printf("enter the number of pages\n");
-    scanf("%d",&n)
+    scanf("%d",&n);
 
-    printf("enter the page sequence");
+    printf("enter the page sequence\n");
     for(int i=0;i<n;i++)
     {
         scanf("%d",&pages[i]);
     }
 
     initialize();
-    fifo(n,pages)
+    fifo(n,pages);
 
 
     return 0;
