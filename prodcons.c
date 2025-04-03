ed3c6076ca14b8ecfd1237a3ed66497b028f7703
diyajojo <diyaj2122@gmail.com>
2025-04-03 14:57:49 +0530

    added comments to understand better

--- prodcons.c
+++ prodcons.c
@@ -1,73 +1,79 @@
 #include <stdio.h>
-#include <stdlib.h>  // for rand() to generate random numbers
-#include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h> // to call the sleep function
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-#define SIZE 5
+# define BUFFER 5
 
-int buffer[SIZE];
-int in = 0, out = 0;
+int in =0, out=0;
+int buffer[BUFFER];
 
-sem_t mutex, full, empty;
+pthread_t prod,cons;
+sem_t mutex,empty,full;
 
-// function both return NULL becuase return type is void* and they are called by the threads
-void *producer(void *arg) 
+
+
+void * producer(void *arg)
 {
-    for (int i = 0; i < 5; i++) {
-        int item = rand() % 100; // Generate a random item
-
-        sem_wait(&empty);
-        sem_wait(&mutex);
-
-        buffer[in] = item;
-        printf("Item produced: %d\n", item);
-        in = (in + 1) % SIZE; // Circular buffer
-
-        sem_post(&mutex);
-        sem_post(&full);
-
-        sleep(1);
-    }
-    return NULL;
+  for(int i=0;i<5;i++)
+  {
+      int item=rand()%100;
+      printf("producer produced: %d\n",item);
+      
+      sem_wait(&empty);
+      sem_wait(&mutex);
+      
+      buffer[in]=item;
+      in= (in+1) % BUFFER;
+      
+      sem_post(&mutex);
+      sem_post(&full);
+      
+      sleep(1);
+      
+  }
+  
+  return NULL;
 }
 
-void *consumer(void *arg) {
-    for (int i = 0; i < 5; i++) {
+void * consumer(void * arg)
+{
+    for(int i=0;i<5;i++)
+    {
         sem_wait(&full);
         sem_wait(&mutex);
-
-        int item = buffer[out]; // Read item from buffer
-        printf("Item consumed: %d\n", item);
-        out = (out + 1) % SIZE; // Circular buffer
-
+        
+        int item=buffer[out];
+        printf("consumer consumed:%d\n",item);
+        out=(out+1)% BUFFER;
+        
         sem_post(&mutex);
         sem_post(&empty);
-
+        
         sleep(1);
     }
+    
     return NULL;
 }
 
-int main() {
+int main()
+{
+    // second parameter is 0 , because all the threads are in one process
 
-    pthread_t prod, cons;
-
-
-    sem_init(&mutex, 0, 1); // 1 indicates mutex is available
-    sem_init(&empty, 0, 0); // 0 indicates buffer is empty
-    sem_init(&full, 0, SIZE);// SIZE indicates buffer is full , the max value taken by full 
-
-// producer and cosnumer are the functions that are called by the threads with NULL arguements
-    pthread_create(&prod, NULL, producer, NULL);
-    pthread_create(&cons, NULL, consumer, NULL);
-
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
-
+    sem_init(&mutex,0,1); // 3rd parameter is one indicating the mutex is available initially
+    sem_init(&empty,0,BUFFER); // 3rd parameter is BUFFER , means initilal empty slots in the buffer
+    sem_init(&full,0,0); // 3rd parameter is 0, means no slots are full initially
+    
+    pthread_create(&prod,NULL,producer,NULL);
+    pthread_create(&cons,NULL,consumer,NULL);
+    
+    pthread_join(prod,NULL);
+    pthread_join(cons,NULL);
+    
     sem_destroy(&mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
-
+    
     return 0;
 }
