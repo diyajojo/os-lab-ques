1e5a9b4110c7fe635e692005f7d6500940083959
diyajojo <diyaj2122@gmail.com>
2025-04-07 18:34:48 +0530

    linked list-bestfit

--- memoryallocation/firstfit.c
+++ memoryallocation/firstfit.c
@@ -1,107 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *head=NULL;
-
 struct node 
 {
     int index;
     int size;
-    struct node* next;
-}
+    struct node * next;
+};
 
+struct node* head=NULL;
 
-void insertMemoryblock(struct node** head, int size, int index)
+void insertMemoryblock(struct node ** head , int size , int index)
 {
-    struct node* newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->index=index;
-    newnode->size=size;
-    newnode->next=NULL;
-
+    struct node* newblock=(struct node*)malloc(sizeof(struct node));
+    newblock->size=size;
+    newblock->index=index;
+    newblock->next=NULL;
+    
     if(*head==NULL)
     {
-        *head=newnode;
+        *head=newblock;
     }
     else
     {
-        struct node * temp =head;
+        struct node *temp =*head;
         while(temp->next!=NULL)
         {
             temp=temp->next;
         }
-        temp->next=newnode;
+        temp->next=newblock;
     }
-
 }
 
-void firstFit(struct node* head,int processSize[],int np)
+void firstFit(struct node * head, int processSize[],int np)
 {
-  
-  int allocated[np];
-  for(int i=0;i<np;i++)
-  {
-    allocated[i]=-1;
-  }
-
-  for (int i=0;i<np;i++)
-  {
-    struct node*temp=head;
-    while(temp!=NULL)
+    int allocated[np];
+    for(int i=0;i<np;i++)
     {
-        if(temp->size>=processSize[i])
+        allocated[i]=-1;
+    }
+    
+    for(int i=0;i<np;i++)
+    {
+        struct node *temp =head;
+        while(temp!=NULL)
         {
-            temp->size-=processSize[i];
-            allocated[i]=temp->index;
-            break;
+            if(temp->size>=processSize[i])
+            {
+                temp->size-=processSize[i];
+                allocated[i]=(temp->index)+1;
+                break;
+            }
+            temp=temp->next;
         }
-        temp=temp->next;
     }
-  }
+    
+    printf("process number\tblock number\n");
+    for(int i=0;i<np;i++)
+    {
+        printf("%d\t\t\t\t",processSize[i]);
+        if(allocated[i]!=-1)
+        {
+            printf("%d\n",allocated[i]);
+        }
+        else
+        {
+            printf("memory not allocated\n");
+        }
+    }
 
-  printf("process-size\t\tblock-no\n");
-  for(int i=0;i<np;i++)
-  {
-    printf("%d\t\t\t\t",processSize[i]);
-    if (allocated[i]!=1)
-    {
-        printf("%d",allocated[i]);
-    }
-    else
-    {
-        printf("process not allocated memory yet");
-    }
-  }
 }
-
 
 
 int main()
 {
-  int np,nb;
-
-  printf("enter the number of memory blocks\n");
-  scanf("%d",&nb);
-
-  for(int i=0;i<nb;i++)
-  {
-    int size;
-    printf("enter the size of the memory block%d\n",(i+1));
-    scanf("%d",&size);
-    insertMemoryblock(&head,size,i);
-  }
-
-  printf("enter the process size\n");
-  scanf("%d",&np);
-
-  int processSize[np];
-
-  for(int i=0;i<np;i++)
-  {
-    printf("enter the size of process %d\n",(i+1));
-    scanf("%d",&processSize[i]);
-  }
-
-  firstFit(head,processSize,np)
-
-  return 0;
+    int nb,np;
+    
+    printf("enter the number of memory blocks\n");
+    scanf("%d",&nb);
+    
+    for(int i=0;i<nb;i++)
+    {
+        int size;
+        printf("enter the size of memory block %d\n",(i+1));
+        scanf("%d",&size);
+        insertMemoryblock(&head,size,i);
+    }
+    
+    printf("enter the number of processes\n");
+    scanf("%d",&np);
+    
+    int processSize[np];
+    
+    for(int i=0;i<np;i++)
+    {
+        printf("enter the size of process %d\n",(i+1));
+        scanf("%d",&processSize[i]);
+    }
+    
+    firstFit(head,processSize,np);
+    return 0;
 }
