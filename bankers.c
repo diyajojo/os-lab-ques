844fcfe4c2241bd1462a18d9ca230b26e5f1f1f7
diyajojo <diyaj2122@gmail.com>
2025-04-08 08:34:43 +0530

    modified

--- bankers.c
+++ bankers.c
@@ -1,91 +1,92 @@
 #include <stdio.h>
 
-#define MAX 10
+#define MAX 100
 
-int max[MAX][MAX], alloc[MAX][MAX], avail[MAX], tempavail[MAX], need[MAX][MAX], finish[MAX];
-int p = 0, r = 0, canRun = 0;
-int count = 0;
+int max[MAX][MAX],alloc[MAX][MAX],avail[MAX],tempavail[MAX],finish[MAX],need[MAX][MAX];
+int r,p, done =0 ,count =0,canRun=0;
 
-int Safe(int p, int r, int tempavail[], int finish[]) 
+
+void isSafe(int p,int r,int tempavail[],int finish[])
 {
-    int done;
-    count = 0;
-
-    while (count < p) {
-        done = 0;
-        for (int i = 0; i < p; i++)
-         {
-            if (finish[i] == 0) 
+    while(count <p)
+    {
+        done=0;
+        for(int i=0;i<p;i++)
+        {
+            if(finish[i]==0)
             {
-                canRun = 1;
+                canRun=1;
                 
-                // Calculate need matrix
-                for (int j = 0; j < r; j++) 
+                for(int j=0;j<r;j++)
                 {
-                    need[i][j] = max[i][j] - alloc[i][j];
-                    if (need[i][j] > tempavail[j]) 
+                    need[i][j]=max[i][j]-alloc[i][j];
+                    if(need[i][j]>tempavail[j])
                     {
-                        canRun = 0;
+                        canRun=0;
                         break;
                     }
+                
                 }
-
-                if (canRun == 1) 
+                if(canRun==1)
                 {
-                    // Process can execute
-                    for (int j = 0; j < r; j++)
-                     {
-                        tempavail[j] += alloc[i][j];
+                    for(int j=0;j<r;j++)
+                    {
+                        tempavail[j]+=alloc[i][j];
                     }
-                    finish[i] = 1;
+                    
+                    finish[i]=1;
                     count++;
-                    done = 1;
+                    done++;
                 }
             }
         }
-        // exits the entire safe function if deadlock is detected
-        if (done == 0)
-            return 0;  // Deadlock detected
+        
+        if(done==0)
+        {
+            printf("deadlock detected\n");
+        }
+        else
+        {
+            printf("deadlock not detected\n");
+        }
     }
-    // at the end of while loop, if all processes are finished
-    return 1;  // No deadlock
 }
 
-int main() {
-
-    printf("Enter number of processes and resources:\n");
-    scanf("%d%d", &p, &r);
-
-    printf("Enter max resource matrix:\n");
-    for (int i = 0; i < p; i++) {
-        for (int j = 0; j < r; j++) {
-            scanf("%d", &max[i][j]);
+int main()
+{
+    printf("enter the number of process and resources\n");
+    scanf("%d%d",&p,&r);
+    
+    printf("enter the maximum resources matrix\n");
+    for(int i=0;i<p;i++)
+    {
+        for(int j=0;j<r;j++)
+        {
+            scanf("%d",&max[i][j]);
         }
     }
-
-    printf("Enter allocated resource matrix:\n");
-    for (int i = 0; i < p; i++) {
-        for (int j = 0; j < r; j++) {
-            scanf("%d", &alloc[i][j]);
+    
+    printf("enter the allocated resource matrix\n");
+    for(int i=0;i<p;i++)
+    {
+        for(int j=0;j<r;j++)
+        {
+            scanf("%d",&alloc[i][j]);
         }
     }
-
-    printf("Enter available resources:\n");
-    for (int i = 0; i < r; i++) 
+    
+    printf("enter the available resources\n");
+    for(int i=0;i<r;i++)
     {
-        scanf("%d", &avail[i]);
-        tempavail[i] = avail[i];
+        scanf("%d",&avail[i]);
+        tempavail[i]=avail[i];
     }
-
-    for (int i = 0; i < p; i++) 
+    
+    for(int i=0;i<p;i++)
     {
-        finish[i] = 0;
+        finish[i]=0;
     }
-
-    if (Safe(p, r, tempavail, finish))
-        printf("Deadlock not present\n");
-    else
-        printf("Deadlock detected\n");
-
+    
+    isSafe (p,r,tempavail,finish);
     return 0;
 }
