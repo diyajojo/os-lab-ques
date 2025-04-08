4a9686d220eedf89eafeb0b91f1e3c3eefc380e8
diyajojo <diyaj2122@gmail.com>
2025-04-08 14:59:06 +0530

    lfu

--- pagereplacement/lru.c
+++ pagereplacement/lru.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 
-#define MAX_FRAMES 3
+#define FRAME 3
 
-int frames[MAX_FRAMES],counter[MAX_FRAMES];
-int found=0;
-int page_faults;
+int frames[FRAME],counter[FRAME],n;
+int pagefault=0,pagehit=0;
 
-void initialize()
+void displayFrames()
 {
-    for(int i=0;i<MAX_FRAMES;i++)
+    for(int j=0;j<FRAME;j++)
     {
-        frames[i]=-1;
-        counter[i]=0;
+        if (frames[j]==-1)
+        {
+            printf("-1\t\t\t");
+        }
+        else
+        {
+            printf("%d\t\t\t",frames[j]);
+        }
     }
+    printf("\n");
 }
 
 int findLRU()
 {
-    int lru=0;
-    for(int i=1;i<MAX_FRAMES;i++)
+    int max=counter[0];
+    int lruIndex=0;
+    
+    for(int j=1;j<FRAME;j++)
     {
-        if(counter[i]>counter[lru])
+        if(counter[j]>max)
         {
-            lru=i;
+            max=counter[j];
+            lruIndex=j;
         }
     }
-    return lru;
-}
-
-void displayFrames()
-{
-    for(int i=0;i<MAX_FRAMES;i++)
-    {
-        if(frames[i]==-1)
-        {
-            printf("%d\t",-1);
-        }
-        else
-        {
-            printf("%d\t",frames[i]);
-        }
-    }
-    printf("\n");
+    return lruIndex;
 }
 
 void LRU(int n,int pages[])
@@ -49,34 +42,38 @@ void LRU(int n,int pages[])
     for(int i=0;i<n;i++)
     {
         int page=pages[i];
-        found=0;
-        for(int j=0;j<MAX_FRAMES;j++)
+        int flag=0;
+        for(int j=0;j<FRAME;j++)
         {
             if(frames[j]==page)
             {
-                found=1;
+                pagehit++;
+                flag=1;
                 break;
             }
         }
-        if(found==0)
+        if(flag==0)
         {
-            int lru=findLRU();
-            frames[lru]=page;
-            counter[lru]=0;
-            page_faults++;
+            int lruIndex=findLRU();
+            frames[lruIndex]=page;
+            counter[lruIndex]=0;
+            pagefault++;
+            
         }
-        for(int i=0;i<MAX_FRAMES;i++)
+        
+        for(int i=0;i<FRAME;i++)
         {
             counter[i]++;
         }
-        for(int i=0;i<MAX_FRAMES;i++)
+        
+        for(int i=0;i<FRAME;i++)
         {
-            if(pages[i]==page)
+            if(frames[i]==page)
             {
                 counter[i]=0;
+                break;
             }
         }
-        
         displayFrames();
     }
 }
@@ -84,10 +81,10 @@ void LRU(int n,int pages[])
 
 int main()
 {
-    int n,pages[10];
-    
     printf("enter the number of pages\n");
     scanf("%d",&n);
+    
+    int pages[n];
     
     printf("enter the page sequence\n");
     for(int i=0;i<n;i++)
@@ -95,147 +92,14 @@ int main()
         scanf("%d",&pages[i]);
     }
     
-    initialize();
+    for(int i=0;i<FRAME;i++)
+    {
+        frames[i]=-1;
+        counter[i]=0;
+    }
+    
     LRU(n,pages);
-
-    return 0;
     
+    printf("page hit is %d\n",pagehit);
+    printf("page fault is %d\n",pagefault);
 }
-
-
-#include <stdio.h>
-#include <stdbool.h>
-
-#define P 5  
-#define R 3 
-
-bool isSafe(int avail[], int max[][R], int allot[][R]) 
-{
-    int work[R];
-    bool finish[P] = {0};
-    int safeSeq[P];
-    int count = 0;
-    
-    for (int i = 0; i < R; i++) 
-        work[i] = avail[i];
-    
-    int need[P][R];
-    for (int i = 0; i < P; i++)
-        for (int j = 0; j < R; j++)
-            need[i][j] = max[i][j] - allot[i][j];
-    
-    bool found;
-    do 
-    {
-        found = false;
-        for (int p = 0; p < P; p++) 
-        {
-            if (finish[p]) 
-                continue;
-            bool canAllocate = true;
-            for (int j = 0; j < R; j++) 
-            {
-                if (need[p][j] > work[j]) 
-                {
-                    canAllocate = false;
-                    break;
-                }
-            }
-            if (canAllocate) 
-            {
-                for (int j = 0; j < R; j++) 
-                    work[j] += allot[p][j];
-                
-                safeSeq[count++] = p;
-                finish[p] = true;
-                found = true;
-            }
-        }
-    }
-     while (found && count < P);
-    if (count < P) {
-        return false;
-    }
-    printf("Safe sequence: ");
-    for (int i = 0; i < P; i++)
-        printf("%d ", safeSeq[i]);
-    printf("\n");
-    return true;
-}
-bool requestResources(int processID, int request[], int avail[], int max[][R], int allot[][R]) 
-{
-    int need[P][R];
-    for (int i = 0; i < P; i++)
-        for (int j = 0; j < R; j++)
-            need[i][j] = max[i][j] - allot[i][j];
-    for (int i = 0; i < R; i++) 
-    {
-        if (request[i] > need[processID][i]) 
-        {
-            printf("Error: Process %d has exceeded its remaining need.\n", processID);
-            printf("Request: %d, Need: %d for resource %d\n", 
-                   request[i], need[processID][i], i);
-            return false;
-        }
-    }
-    for (int i = 0; i < R; i++) 
-    {
-        if (request[i] > avail[i]) 
-        {
-            printf("Error: Resources not available for Process %d.\n", processID);
-            return false;
-        }
-    }
-    for (int i = 0; i < R; i++) 
-    {
-        avail[i] -= request[i];
-        allot[processID][i] += request[i];
-    }
-    if (isSafe(avail, max, allot)) 
-    {
-        printf("Request granted for Process %d.\n", processID);
-        return true;
-    } 
-    else 
-    {
-        printf("Request denied for Process %d. System would be in an unsafe state.\n", processID);
-        for (int i = 0; i < R; i++) 
-        {
-            avail[i] += request[i];
-            allot[processID][i] -= request[i];
-        }
-        return false;
-    }
-}
-
-void displayState(int avail[], int max[][R], int allot[][R]) 
-{
-    int need[P][R];
-    for (int i = 0; i < P; i++)
-        for (int j = 0; j < R; j++)
-            need[i][j] = max[i][j] - allot[i][j];
-    printf("\nCurrent System State:\n");
-    printf("Process\tAllocation\tMax\tNeed\tAvailable\n");
-    for (int i = 0; i < P; i++) 
-    {
-        printf("P%d\t", i);
-        for (int j = 0; j < R; j++)
-            printf("%d ", allot[i][j]);
-        printf("\t\t");
-        for (int j = 0; j < R; j++)
-            printf("%d ", max[i][j]);
-        printf("\t");
-        for (int j = 0; j < R; j++)
-            printf("%d ", need[i][j]);
-        if (i == 0) 
-        {
-            printf("\t");
-            for (int j = 0; j < R; j++)
-                printf("%d ", avail[j]);
-        }
-        
-        printf("\n");
-    }
-    printf("\n");
-}
-
